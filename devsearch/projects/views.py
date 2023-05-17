3705d727fead8a426dc2ac5f60adb8d4479d459a
Haruto Mori <baystarsman@outlook.jp>
2023-05-17 21:44:21 +0900

    feat: add data to render

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -4,7 +4,15 @@ from django.http import HttpResponse
 
 
 def projects(request):
-    return render(request, 'projects/projects.html')
+    page = 'projects page'
+    number = 100
+
+    context = {
+        'page': page,
+        'number': number,
+    }
+
+    return render(request, 'projects/projects.html', context)
 
 def project(request, pk):
     return render(request, 'projects/single-project.html')
