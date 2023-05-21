fe2d1226454094b6da2c4baa712a1b80772d197e
Haruto Mori <baystarsman@outlook.jp>
2023-05-22 01:53:08 +0900

    feat: add users profile views and its url

--- devsearch/users/views.py
+++ devsearch/users/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def profiles(request):
+
+    context = {
+        
+    }
+
+    return render(request, 'users/profiles.html', context)
