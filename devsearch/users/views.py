9674ce4b912bbc0949caf76d05382fdcee2702fe
Haruto Mori <baystarsman@outlook.jp>
2023-05-24 14:23:40 +0900

    feat: add custom user creation form instead of default one

--- devsearch/users/views.py
+++ devsearch/users/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators  import login_required
 from django.contrib import messages
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 from .models import Profile
+from .forms import CustomUserCreationForm
 
 
 def login_user(request):
@@ -46,10 +46,10 @@ def logout_user(request):
 
 def register_user(request):
     page = 'register'
-    form = UserCreationForm()
+    form = CustomUserCreationForm()
 
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
 
         if form.is_valid():
             user = form.save(commit=False)
