e83add1aedaaeebe2b5da31eab4b6d426c9c9f5f
Haruto Mori <baystarsman@outlook.jp>
2023-05-24 14:15:42 +0900

    feat: add if statement to creat new user

--- devsearch/users/views.py
+++ devsearch/users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators  import login_required
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 from .models import Profile
@@ -45,9 +46,27 @@ def logout_user(request):
 
 def register_user(request):
     page = 'register'
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+
+            messages.success(request, 'User account was created!')
+
+            login(request, user)
+            return redirect('profiles') 
+        
+        else:
+            messages.error(request, 'An error has occured during registration')
 
     context = {
         'page': page,
+        'form': form,
     }
 
     return render(request, 'users/login_register.html', context)
