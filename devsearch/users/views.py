a3557b9d5862c1ff63c5c0380e9f918bc8aeefe7
Haruto Mori <baystarsman@outlook.jp>
2023-05-26 16:41:51 +0900

    feat: add profile, project and skills data for user account page

--- devsearch/users/views.py
+++ devsearch/users/views.py
@@ -96,9 +96,17 @@ def user_profile(request, pk):
 
     return render(request, 'users/user_profile.html', context)
 
-
+@login_required(login_url='login')
 def user_account(request):
+    profile = request.user.profile
 
-    context = {}
+    skills = profile.skill_set.all()
+    projects = profile.project_set.all()
+
+    context = {
+        'profile': profile,
+        'skills': skills,
+        'projects': projects,
+    }
 
     return render(request, 'users/account.html', context)
