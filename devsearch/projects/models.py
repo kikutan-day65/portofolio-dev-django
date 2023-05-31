3cb741c5afe0be378b067b6816e846ec63d18ca8
Haruto Mori <baystarsman@outlook.jp>
2023-05-31 13:01:30 +0900

    feat: change the order based on created date for projects

--- devsearch/projects/models.py
+++ devsearch/projects/models.py
@@ -29,6 +29,10 @@ class Project(models.Model):
     def __str__(self):
         return self.title
     
+    class Meta:
+        ordering = ['created']
+
+    
 class Review(models.Model):
     VOTE_TYPE = (
         ('up', 'Up Vote'),
