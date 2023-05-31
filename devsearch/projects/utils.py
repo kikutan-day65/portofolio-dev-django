57b3e8f72389ad7050fc2dcf745c6913b72f669c
Haruto Mori <baystarsman@outlook.jp>
2023-05-31 14:49:09 +0900

    feat: create a pagination funciton for projects in util

--- devsearch/projects/utils.py
+++ devsearch/projects/utils.py
@@ -1,6 +1,38 @@
 from django.db.models import Q
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 from .models import Project, Tag
+
+
+def pagination_projects(request, projects, results):
+
+    page = request.GET.get('page')
+    results = 3
+    paginator = Paginator(projects, results)
+
+    try:
+        projects = paginator.page(page)
+    except PageNotAnInteger:
+        page = 1
+        projects = paginator.page(page)
+    except EmptyPage:
+        page = paginator.num_pages
+        projects = paginator.page(page)
+
+    left_index = (int(page) - 4)
+
+    if left_index < 1:
+        left_index = 1
+    
+    right_index = (int(page) + 5)
+
+    if right_index > paginator.num_pages:
+        right_index = paginator.num_pages + 1
+
+    custom_range = range(left_index, right_index)
+
+
+    return custom_range, projects
 
 
 def search_projects(request):
