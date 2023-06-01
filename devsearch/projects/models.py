c7bef016cf8f1615b35e80bb2197576e6e86b8c2
Haruto Mori <baystarsman@outlook.jp>
2023-06-01 20:56:31 +0900

    feat: count vote total and ratio

--- devsearch/projects/models.py
+++ devsearch/projects/models.py
@@ -32,6 +32,18 @@ class Project(models.Model):
     class Meta:
         ordering = ['created']
 
+    @property
+    def get_vote_count(self):
+        reviews = self.review_set.all()
+        up_votes = reviews.filter(value='up').count()
+        total_votes = reviews.count()
+
+        ratio = (up_votes / total_votes) * 100
+        self.vote_total = total_votes
+        self.vote_ratio = ratio
+
+        self.save()
+
     
 class Review(models.Model):
     VOTE_TYPE = (
