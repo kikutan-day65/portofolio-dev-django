497ca3ec2687c7337bfe5ee21a2f0ad594988aef
Haruto Mori <baystarsman@outlook.jp>
2023-06-01 21:58:19 +0900

    fix: fix the fields for subject

--- devsearch/users/models.py
+++ devsearch/users/models.py
@@ -55,7 +55,7 @@ class Message(models.Model):
     recipient = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True, blank=True, related_name='messages')
     name = models.CharField(max_length=200, null=True, blank=True)
     email = models.EmailField(max_length=200, null=True, blank=True)
-    subject = models.EmailField(max_length=200, null=True, blank=True)
+    subject = models.CharField(max_length=200, null=True, blank=True)
     body = models.TextField()
     is_read = models.BooleanField(default=False, null=True)
     created = models.DateTimeField(auto_now_add=True)
