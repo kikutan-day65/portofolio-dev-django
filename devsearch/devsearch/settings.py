a6d97ece4c91343fcb5ef42d8c99d64b1303ab43
Haruto Mori <baystarsman@outlook.jp>
2023-06-02 14:14:19 +0900

    feat: add email sending setting (outlook server used)

--- devsearch/devsearch/settings.py
+++ devsearch/devsearch/settings.py
@@ -119,6 +119,13 @@ USE_I18N = True
 
 USE_TZ = True
 
+# Sending emails
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.office365.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
