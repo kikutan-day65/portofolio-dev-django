ac7fe1c71067a0e681d4a401b76306aeaf433b14
Haruto Mori <baystarsman@outlook.jp>
2023-06-02 17:24:12 +0900

    feat: change the email to gmail

--- devsearch/devsearch/settings.py
+++ devsearch/devsearch/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 
 # Sending emails
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.office365.com'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
