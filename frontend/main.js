c12f4f36da4c65784b542a4370828d59837b8285
Haruto Mori <baystarsman@outlook.jp>
2023-06-07 02:05:18 +0900

    style: change the style of frontend page for api

--- frontend/main.js
+++ frontend/main.js
@@ -14,14 +14,25 @@ let get_projects = () => {
 }
 
 let build_projects = (projects) => {
-    let projects_wrapper = document.getElementById('projects-wrapper')
+    let projects_wrapper = document.getElementById('projects--wrapper')
 
     for (let i = 0; projects.length > i; i++){
         let project = projects[i]
         
         let project_card = `
-            <div>
-                <p>${project.title}</p>
+            <div class="project--card">
+                <img src="http://127.0.0.1:8000${ project.featured_image }">
+
+                <div>
+                    <div class="card--header">
+                        <h3>${ project.title }</h3>
+                        <strong class="vote--option">&#43;</strong>
+                        <strong class="vote--option">&#8722;</strong>
+                    </div>
+                    <i>${ project.vote_ratio }% Positive feedback</i>
+                    <p>${ project.description.substring(0, 150) }</p>
+                </div>
+
             </div>
         `
         projects_wrapper.innerHTML += project_card
