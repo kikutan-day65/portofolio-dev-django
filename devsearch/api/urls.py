5034bb32e5084fd97910efb085afa61e8fe18cd1
Haruto Mori <baystarsman@outlook.jp>
2023-06-06 17:49:44 +0900

    feat: add simple JWT deafult urls

--- devsearch/api/urls.py
+++ devsearch/api/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
 from . import views
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+
 urlpatterns = [
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
     path('', views.get_routes),
     path('projects/', views.get_projects),
     path('projects/<str:pk>/', views.get_project),
