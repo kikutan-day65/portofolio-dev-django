bcca259633267dba66cfcfca65ccfcc0bb24f3fc
Haruto Mori <baystarsman@outlook.jp>
2023-06-06 16:16:29 +0900

    feat: include api urls

--- devsearch/devsearch/urls.py
+++ devsearch/devsearch/urls.py
@@ -15,6 +15,8 @@ urlpatterns = [
     path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name='reset_password_sent.html'), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='reset.html'), name='password_reset_confirm'),
     path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='reset_password_complete.html'), name='password_reset_complete'),
+
+    path('api/', include('api.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
