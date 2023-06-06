b5e5f400d95244f77016d16ea8e99d29e660225e
Haruto Mori <baystarsman@outlook.jp>
2023-06-07 02:41:28 +0900

    style: enable to send POST data from frontend

--- frontend/main.js
+++ frontend/main.js
@@ -15,6 +15,7 @@ let get_projects = () => {
 
 let build_projects = (projects) => {
     let projects_wrapper = document.getElementById('projects--wrapper')
+    projects_wrapper.innerHTML = ''
 
     for (let i = 0; projects.length > i; i++){
         let project = projects[i]
@@ -26,8 +27,8 @@ let build_projects = (projects) => {
                 <div>
                     <div class="card--header">
                         <h3>${ project.title }</h3>
-                        <strong class="vote--option">&#43;</strong>
-                        <strong class="vote--option">&#8722;</strong>
+                        <strong class="vote--option" data-vote="up" data-project="${ project.id }">&#43;</strong>
+                        <strong class="vote--option" data-vote="down" data-project="${ project.id }">&#8722;</strong>
                     </div>
                     <i>${ project.vote_ratio }% Positive feedback</i>
                     <p>${ project.description.substring(0, 150) }</p>
@@ -36,6 +37,40 @@ let build_projects = (projects) => {
             </div>
         `
         projects_wrapper.innerHTML += project_card
+    }
+
+    add_vote_events()
+
+}
+
+let add_vote_events = ()=> {
+    let vote_btns = document.getElementsByClassName('vote--option')
+    
+    for (let i = 0; vote_btns.length > i; i++) {
+
+        vote_btns[i].addEventListener('click', (e) => {
+            let token = localStorage.getItem('token')
+            console.log('TOKEN:', token)
+
+            let vote = e.target.dataset.vote
+            let project = e.target.dataset.project
+
+            fetch(`http://127.0.0.1:8000/api/projects/${project}/vote/`, {
+                method: 'POST', 
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${token}`,
+                },
+                body: JSON.stringify({'value':vote})
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Success:', data)
+                get_projects()
+            })
+
+        })
+
     }
 }
 
