b0bcfa159549c41c9e4da85c7262248e5da9f7a1
Haruto Mori <baystarsman@outlook.jp>
2023-06-08 09:40:40 +0900

    feat: render the page if project image is deleted

--- devsearch/projects/models.py
+++ devsearch/projects/models.py
@@ -31,6 +31,15 @@ class Project(models.Model):
     
     class Meta:
         ordering = ['-vote_ratio', '-vote_total', 'title']
+
+    @property
+    def image_url(self):
+        try:
+            url = self.featured_image.url
+        except:
+            url = ''
+            
+        return url
     
     @property
     def reviewers(self):
