47e434be7a874a42ceef6f12fa013577544465ee
Haruto Mori <baystarsman@outlook.jp>
2023-06-08 11:08:26 +0900

    style: add js to delete tags

--- devsearch/static/js/main.js
+++ devsearch/static/js/main.js
@@ -20,3 +20,28 @@ if (searchForm) {
         })
     }
 }
+
+
+let tags = document.getElementsByClassName('project-tag')
+
+    for (let i = 0; tags.length > i; i++) {
+        tags[i].addEventListener('click', (e) => {
+            let tag_id = e.target.dataset.tag
+            let project_id = e.target.dataset.project
+
+            // console.log('TAG ID:', tag_id)
+            // console.log('PROJECT ID:', project_id)
+
+            fetch('http://127.0.0.1:8000/api/remove-tag/', {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({'project': project_id, 'tag': tag_id})
+            })
+            .then(response => response.json())
+            .then(data => {
+                e.target.remove()
+            })
+        })
+    }
